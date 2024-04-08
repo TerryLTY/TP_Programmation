1a7f80426cacb3f1659aa1822193eeb61d4c4972
TerryLTY <143763387+TerryLTY@users.noreply.github.com>
2024-04-08 10:11:53 -0400

    Update course.js

--- course/course.js
+++ course/course.js
@@ -1,1 +1,27 @@
+let voiture1 = document.getElementById(voiture1).innerHTML;
+let voiture2 = document.getElementById(voiture2).innerHTML;
+let translationX1;
+let translationX2;
+let containerWidth;
+let voiture1Width;
+let voiture2Width;
 
+function compteARebour() {
+
+}
+
+function chronometre() {
+    
+}
+
+function aleatoireTranslationX() {
+
+}
+
+function deplacerVoiture() {
+
+}
+
+function demarrerCourse() {
+
+}

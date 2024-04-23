d58f454eed2c128065b54324f44743e977f717a8
TerryLTY <143763387+TerryLTY@users.noreply.github.com>
2024-04-23 18:57:25 -0400

    Update course.js

--- course/course.js
+++ course/course.js
@@ -1,32 +1,45 @@
-let voiture1;
-let voiture2;
+let voiture1 = 80;
+let voiture2 = 80;
+let containerWidth = 1200;
 let translationX1;
 let translationX2;
-let containerWidth;
-let voiture1Width;
-let voiture2Width;
-let countdown = 10;
+let maxTranslation = containerWidth - voiture1;
+let decompte = 10;
 
 function compteARebour() {
-    if (countdown > 0) {
-        countdown --;
-        document.getElementById("countdown").innerHTML = countdown;
+    if (decompte > 1) {
+        decompte --;
+        document.getElementById("decompte").innerHTML = decompte;
+    } else {
+        document.getElementById("decompte").innerHTML = "GO!";
     }
+    return decompte;
 }
-setInterval(compteARebour, 1000)
 
 function chronometre() {
     
 }
 
 function aleatoireTranslationX() {
-
+    translationX1 = Math.floor(Math.random() * 100);
+    translationX2 = Math.floor(Math.random() * 100);
 }
 
 function deplacerVoiture() {
-
+    voiture1 += translationX1;
+    voiture2 += translationX2;
+    document.getElementById("voiture1").style.translate = voiture1 + "px"; 
+    document.getElementById("voiture2").style.translate = voiture2 + "px";
+    //style.translate a la place de style.left pour une animation lisse (c'est plus cool)//
 }
 
 function demarrerCourse() {
+    setInterval(compteARebour, 500);
+    setInterval(aleatoireTranslationX,500);
+    setInterval(deplacerVoiture, 500);
+    
+}
 
+function arreterCourse() {
+    clearInterval()
 }

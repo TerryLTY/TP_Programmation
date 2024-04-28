aaab898c5c14eee16de618cb6336891ff79123db
ThearyL <142918338+ThearyL@users.noreply.github.com>
2024-04-28 18:49:27 -0400

    Update course.js

--- course/course.js
+++ course/course.js
@@ -5,7 +5,7 @@ let translationX1;
 let translationX2;
 let maxTranslation = containerWidth - voiture1;
 let decompte = 10;
-let tempsDepart
+let tempsDepart;
 
 function compteARebour() {
    
@@ -27,9 +27,9 @@ function compteARebour() {
 
 function chronometre() { 
      let temps = Date.now() - tempsDepart;   
-    secondes = Math.floor((temps/1000) % 60);
-    document.getElementById("chronometre1").innerHTML =  (secondes < 10 ? "0":"0") + secondes + " secondes";
-    document.getElementById("chronometre2").innerHTML = (secondes < 10 ? "0" : "0") + secondes + " secondes";
+    secondes = Math.floor((temps/1000));
+    document.getElementById("chronometre1").innerHTML =  (secondes < 10 ? "0":"") + secondes + " secondes";
+    document.getElementById("chronometre2").innerHTML = (secondes < 10 ? "0" : "") + secondes + " secondes";
 }
 
 function aleatoireTranslationX() {

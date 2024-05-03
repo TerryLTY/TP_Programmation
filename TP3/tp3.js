d0a78a54b0d70aae05e5bcf23180e250a9d3b78a
TerryLTY <143763387+TerryLTY@users.noreply.github.com>
2024-05-03 10:54:56 -0400

    Update tp3.js

--- TP3/tp3.js
+++ TP3/tp3.js
@@ -2,4 +2,12 @@ var titre = document.createElement("H1");
 titre.setAttribute("id", "titre");
 titre.textContent = "Commande de Pizzas";
 
+var foot = document.createElement("DIV");
+var textefoot = document.createElement("p")
+textefoot.textContent = "Kordan Radhouane - Lach Thearylou - Lu Ting Yung";
+textefoot.setAttribute = ("id", "foot");
+
+foot.appendChild(textefoot)
+
 document.body.appendChild(titre);
+document.body.appendChild(foot);
